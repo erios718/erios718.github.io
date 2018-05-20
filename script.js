898adf60dfa3f17aedd7d527157580f1be2d85c7
Edgar Rios <erios718@yahoo.com>
2018-05-20 13:30:22 -0400

    Website upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function loadPage(id){
+	
+	if(id=='HoA')
+	window.location.href="HoC.html";
+	if(id=='PY')
+	window.location.href="PY.html";
+	if(id=='TA')
+	window.location.href="TA.html";
+	if(id=='OR')
+	window.location.href="OR.html";
+	if(id=='BH')
+	window.location.href="index.html";
+}
+
